295ae227f6310c3bbfd571fda212f57476c5662e
darioy1793 <darioy1793@gmail.com>
2026-02-24 09:52:21 -0500

    productos

---
+++ src/screens/HomeScreen/compponents/CardProducctComponents.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const CardProducctComponents = () => {
+  return (
+    <View>
+        <Text>Lista de Productos</Text>
+    </View>
+  )
+}
